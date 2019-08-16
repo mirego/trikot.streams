378a12ebe5dc4db76fe52d7566ea19aff259d3be
Jean-David Tremblay <jdavid.tremblay@gmail.com>
2019-08-16 10:57:51 -0400

    Apply PR comments

--- android-ktx/src/main/java/com/mirego/trikot/streams/android/ktx/PublisherExtensions.kt
+++ android-ktx/src/main/java/com/mirego/trikot/streams/android/ktx/PublisherExtensions.kt
@@ -8,14 +8,12 @@ import org.reactivestreams.Publisher
 
 typealias ObserveBlock<T> = (T) -> Unit
 
-fun <T> Publisher<T>.observe(lifecycleOwner: LifecycleOwner, observeBlock: ObserveBlock<T>) {
-    return asLiveData().observe(lifecycleOwner, observeBlock)
-}
+fun <T> Publisher<T>.observe(lifecycleOwner: LifecycleOwner, observeBlock: ObserveBlock<T>) =
+    asLiveData().observe(lifecycleOwner, observeBlock)
 
-fun <T> LiveData<T>.observe(lifecycleOwner: LifecycleOwner, observeBlock: ObserveBlock<T>) {
-    this.observe(lifecycleOwner, Observer { observeBlock(it) })
-}
+fun <T> LiveData<T>.observe(lifecycleOwner: LifecycleOwner, observeBlock: ObserveBlock<T>) =
+    observe(lifecycleOwner, Observer { observeBlock(it) })
 
-fun <T> Publisher<T>.asLiveData(): LiveData<T> {
-    return LiveDataReactiveStreams.fromPublisher(this)
-}
+
+fun <T> Publisher<T>.asLiveData(): LiveData<T> = LiveDataReactiveStreams.fromPublisher(this)
+

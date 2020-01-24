98fc2e0895774879ad9e2437d24c77e94e1d1469
Pascal Boutin <pboutin@mirego.com>
2020-01-24 16:04:59 -0500

    Add js names to Publishers

--- streams/src/commonMain/kotlin/com/mirego/trikot/streams/reactive/Publishers.kt
+++ streams/src/commonMain/kotlin/com/mirego/trikot/streams/reactive/Publishers.kt
@@ -3,22 +3,27 @@ package com.mirego.trikot.streams.reactive
 import org.reactivestreams.Publisher
 
 object Publishers {
+    @JsName("behaviorSubject")
     fun <T> behaviorSubject(value: T? = null): BehaviorSubject<T> {
         return BehaviorSubjectImpl(value)
     }
 
+    @JsName("publishSubject")
     fun <T> publishSubject(): PublishSubject<T> {
         return PublishSubjectImpl()
     }
 
+    @JsName("just")
     fun <T> just(value: T): Publisher<T> {
         return behaviorSubject(value).also { it.complete() }
     }
 
+    @JsName("empty")
     fun <T> empty(): Publisher<T> {
         return PublishSubjectImpl()
     }
 
+    @JsName("completed")
     fun <T> completed(): Publisher<T> {
         return PublishSubjectImpl<T>().also { it.complete() }
     }

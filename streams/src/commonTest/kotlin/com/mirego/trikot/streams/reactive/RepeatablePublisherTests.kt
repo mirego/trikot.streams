fe4adc275726a479e3898db986f69c3ba13747f6
Raphaël Blanchet <rblanchet@mirego.com>
2021-10-15 14:03:45 -0400

    Use by atomic instead

--- streams/src/commonTest/kotlin/com/mirego/trikot/streams/reactive/RepeatablePublisherTests.kt
+++ streams/src/commonTest/kotlin/com/mirego/trikot/streams/reactive/RepeatablePublisherTests.kt
@@ -1,7 +1,6 @@
 package com.mirego.trikot.streams.reactive
 
-import com.mirego.trikot.foundation.concurrent.AtomicReference
-import com.mirego.trikot.foundation.concurrent.setOrThrow
+import com.mirego.trikot.foundation.concurrent.atomic
 import com.mirego.trikot.streams.cancellable.CancellableManager
 import com.mirego.trikot.streams.utils.MockTimer
 import com.mirego.trikot.streams.utils.MockTimerFactory
@@ -24,9 +23,9 @@ class RepeatablePublisherTests {
 
     @Test
     fun blockIsReexecutedIfResubscribed() {
-        var executionCount = AtomicReference(0)
+        var executionCount by atomic(0)
         val repeatable = Publishers.repeat(Duration.minutes(1)) {
-            executionCount.setOrThrow(executionCount.value + 1)
+            executionCount++
             Publishers.behaviorSubject<String>()
         }
         val cancellableManager = CancellableManager()
@@ -35,25 +34,25 @@ class RepeatablePublisherTests {
         cancellableManager.cancel()
         repeatable.subscribe(CancellableManager()) {}
 
-        assertEquals(2, executionCount.value)
+        assertEquals(2, executionCount)
     }
 
     @Test
     fun blockIsReExecutedWhenRepeated() {
-        var executions = AtomicReference(0)
+        var executions by atomic(0)
         var timer: MockTimer? = null
         val timerFactory = MockTimerFactory { _, duration ->
             assertEquals(Duration.minutes(1), duration)
             MockTimer().also { timer = it }
         }
         val repeatable = Publishers.repeat(Duration.minutes(1), timerFactory) {
-            executions.setOrThrow(executions.value + 1)
+            executions++
             Publishers.behaviorSubject<String>()
         }
 
         repeatable.subscribe(CancellableManager()) {}
         timer?.executeBlock()
 
-        assertEquals(2, executions.value)
+        assertEquals(2, executions)
     }
 }
